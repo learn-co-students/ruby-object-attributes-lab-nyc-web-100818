bae5a5d7313ea715cb2fceed4b2a358ea1d7e2e1
Rexman17 <a.stone1794@gmail.com>
2018-10-09 14:51:41 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  # setter
+  def name=(dog_name) # use arg u passed in
+    @name = dog_name
+  end
+
+  # getter
+  def name
+    @name
+  end
+
+  def breed=(dog_breed) # setter
+    @breed = dog_breed # use argument u passed in
+  end
+
+  def breed # getter 
+    @breed
+  end
+end
